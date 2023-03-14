66ad76639014a0df33639f2a1c09876f8836342c
Andemir-programing <bogatyrevandemir@gmail.com>
2023-03-14 21:30:17 +0300

    fix update

--- tests/users/test_create_user_wrong_fields.py
+++ tests/users/test_create_user_wrong_fields.py
@@ -1,3 +1,4 @@
+# это делаю я
 import pytest
 @pytest.mark.parametrize("username, age, address, accessed_catalog, exp_code", [
     ([1, 2, 3], 12, "grhgn j", {"name": "idk", "catalog": "phones"}, 422),
@@ -6,7 +7,7 @@ import pytest
     ("Ivan", 5, "dfhurieg", ["idk"], 422)
 ])
 
-def test_negative(application, username, age, address, accessed_catalog, exp_code):
-    response = application.api_client.user.create_user(username, age, address, accessed_catalog)
+def test_negative(user_fixture, username, age, address, accessed_catalog, exp_code):
+    response = user_fixture.api_client.user.create_user(username, age, address, accessed_catalog)
 
     assert response.status_code == exp_code, "Статус код не соответствует ожидаемому"
--- tests/users/test_update_user.py
+++ tests/users/test_update_user.py
@@ -1,14 +1,13 @@
-#это делаю я
 import pytest
 
 @pytest.mark.parametrize("username, age, address, accessed_catalog, exp_code", [
-    ('Ivan', 12, "Nalchik", {"name": "Andemir", "catalog": "phones"}, 200),
-    ("Ivan", 32, "Moscow", {"name": "Volodya", "catalog": "food"}, 200),
+    ('Ivan', 12, "Nalchik", {"name": "ghdr", "catalog": "phones"}, 200),
+    ("Ivan", 32, "Moscow", {"name": "", "catalog": "food"}, 200),
     ("Ivan", 4, "Saint Petersburg", {"name": "Ahmed", "catalog": "furniture"}, 200),
     ("Ivan", 5, "Rome", {"name": "Damir", "catalog": "vehicle"}, 200),
     ("Ivan", 28, "Paris", {"name": "Ilyas", "catalog": "international_food"}, 200)
 ])
-def test_positive(application, username, age, address, accessed_catalog, exp_code):
-    response = application.api_client.user.update_user(username, age, address, accessed_catalog)
+def test_positive(user_fixture, username, age, address, accessed_catalog, exp_code):
+    response = user_fixture.api_client.user.update_user(username, age, address, accessed_catalog)
 
     assert response.status_code == exp_code, "Статус код не соответствует ожидаемому"
