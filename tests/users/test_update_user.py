5d699cf99cd97630a8d8911984c89bee8531efcb
Andemir-programing <bogatyrevandemir@gmail.com>
2023-03-15 16:55:16 +0300

    add delete_user

--- tests/users/test_update_user.py
+++ tests/users/test_update_user.py
@@ -1,8 +1,8 @@
 import pytest
 
 @pytest.mark.parametrize("username, age, address, accessed_catalog, exp_code", [
-    ('Ivan', 12, "Nalchik", {"name": "ghdr", "catalog": "phones"}, 200),
-    ("Ivan", 32, "Moscow", {"name": "", "catalog": "food"}, 200),
+    ('Ivan', 12, "Nalchik", {"name": "Andemir", "catalog": "phones"}, 200),
+    ("Ivan", 32, "Moscow", {"name": "Volodya", "catalog": "food"}, 200),
     ("Ivan", 4, "Saint Petersburg", {"name": "Ahmed", "catalog": "furniture"}, 200),
     ("Ivan", 5, "Rome", {"name": "Damir", "catalog": "vehicle"}, 200),
     ("Ivan", 28, "Paris", {"name": "Ilyas", "catalog": "international_food"}, 200)
