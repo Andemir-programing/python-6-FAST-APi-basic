d4dbf628eafe31c458a11e9ef7430ff400a9b031
ilyas <ilyas.niyazov@rt.ru>
2023-03-18 19:09:54 +0300

    fix tests

--- tests/employees/test_delete_employee_wrong_fields.py
+++ tests/employees/test_delete_employee_wrong_fields.py
@@ -5,7 +5,7 @@ import pytest
     ("uncorrect_id", 422),
     ([999999999], 422),
     (9999999999, 404),
-    (-999999, 422)
+    (-999999, 404)
 ])
 def test_negative(employee_fixture, employee_id, code_exp):
     response = employee_fixture.api_client.employee.delete_employee(employee_id)
