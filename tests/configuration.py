9b2f0fa366359bdfee8fd0bb66980e90e51a996f
Andemir-programing <bogatyrevandemir@gmail.com>
2023-03-18 13:07:59 +0300

    add delete employee

--- tests/configuration.py
+++ tests/configuration.py
@@ -1,7 +1,7 @@
 import os
 
 mode = "docker"
-url = "http://192.168.99.100" if mode == "docker" else "http://127.0.0.1"
+url = "http://127.0.0.1"
 token = ""
 base_path = f"{os.path.dirname(os.path.abspath(__file__))}/"
 
