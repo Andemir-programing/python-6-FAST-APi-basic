4d70f7fbea1f4db7df580cbb416d52d35c4e693e
sanomanjiro15 <milana.0070@list.ru>
2023-03-18 22:22:12 +0300

    product conftest,clients

--- tests/configuration.py
+++ tests/configuration.py
@@ -8,6 +8,6 @@ base_path = f"{os.path.dirname(os.path.abspath(__file__))}/"
 
 ACCESSED_CATALOG_ENUM = ["food", "phones", "furniture", "vehicle", "international_food"]
 ROLE_ENUM = ["admin", "seller", "expert"]
+Dimension = ["length", "width", "height"]
 
 
-# new line to branch Ilyas
