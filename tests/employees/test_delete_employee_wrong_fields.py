2c854a94579bf3bacb34d79e0bba201e95cb1369
ilyas <ilyas.niyazov@rt.ru>
2023-03-18 19:04:03 +0300

    add test_delete_employee_wrong_fields.py

--- tests/employees/test_delete_employee_wrong_fields.py
+++ tests/employees/test_delete_employee_wrong_fields.py
@@ -4,7 +4,8 @@ import pytest
 @pytest.mark.parametrize("employee_id, code_exp", [
     ("uncorrect_id", 422),
     ([999999999], 422),
-    (9999999999, 404)
+    (9999999999, 404),
+    (-999999, 422)
 ])
 def test_negative(employee_fixture, employee_id, code_exp):
     response = employee_fixture.api_client.employee.delete_employee(employee_id)
